ab2dab0a78054ce212f817b3d46dbb233fa97506
erom-dm <swiftyerom@gmail.com>
2019-04-14 15:54:49 +0300

    fixed redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, compose } from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducers from "./reducers/reducers.js";
 import middleware from "./middleware/middleware";
+import {composeWithDevTools} from 'redux-devtools-extension'
 import App from "./App";
 
 import "./styles.scss";
 
-const composeEnhancers =
-  (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-    // eslint-disable-next-line
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      trace: true,
-      traceLimit: 25
-    })) ||
-  compose;
-const store = createStore(reducers, middleware);
+const store = createStore(reducers, composeWithDevTools(middleware));
 
 ReactDOM.render(
   <Provider store={store}>
